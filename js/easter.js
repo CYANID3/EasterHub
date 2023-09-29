eae6434a31c646f24f4bce1d3d01e09e7624ae0e
CYANID3 <tomorrrowland.22@gmail.com>
2023-09-29 16:00:00 +0600

    modified:   js/easter.js 	deleted:    music/ntro_music.mp3

--- js/easter.js
+++ js/easter.js
@@ -1,34 +1,44 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let secretCode = [
-        'ArrowUp',
-        'ArrowUp',
-        'ArrowDown',
-        'ArrowDown',
-        'ArrowLeft',
-        'ArrowRight',
-        'ArrowLeft',
-        'ArrowRight',
-        'KeyB',
-        'KeyA'
-    ];
+// Этот блок кода будет выполнен, когда весь HTML-документ будет полностью загружен
+document.addEventListener("DOMContentLoaded", function () {
 
-    let inputCode = [];
-    let audio = new Audio();
-    audio.src = "./music/ntro_music.mp3";
-    audio.preload = 'auto';
+  // Массив с последовательностью клавиш для активации пасхалки
+  // В моём случае это Konami-код
+  let secretCode = [
+    "ArrowUp",
+    "ArrowUp",
+    "ArrowDown",
+    "ArrowDown",
+    "ArrowLeft",
+    "ArrowRight",
+    "ArrowLeft",
+    "ArrowRight",
+    "KeyB",
+    "KeyA",
+  ];
 
-    document.addEventListener('keydown', function(e) {
+  // Массив, в который будут записываться введенные пользователем клавиши
+  let inputCode = [];
+
+  // Этот блок кода будет выполняться при нажатии клавиши на клавиатуре
+  document.addEventListener("keydown", function (e) {
+
+    // Добавляем код нажатой клавиши в массив inputCode
     inputCode.push(e.code);
 
-        if (inputCode.length > secretCode.length) {
-            inputCode.shift();
-        }
+    // Если длина массива inputCode превышает длину secretCode, удаляем первый элемент
+    if (inputCode.length > secretCode.length) {
+      inputCode.shift();
+    }
 
-        if (inputCode.toString() === secretCode.toString()) {
-            audio.play();
-            let hiddenText = document.querySelector('.content__text');
-            hiddenText.textContent = "You find an easter egg";
-            inputCode = [];
-        }
-    });
+    // Если введенная последовательность совпадает с секретной, выполняем необходимые действия
+    // В моем случе это вывод текста в блок .content__text
+    if (inputCode.toString() === secretCode.toString()) {
+
+      let hiddenText = document.querySelector(".content__text");
+      hiddenText.textContent = "You find an easter egg"
+
+      // После выполнения действий очищаем массив inputCode   
+      inputCode = [];
+    }
+  });
 });
